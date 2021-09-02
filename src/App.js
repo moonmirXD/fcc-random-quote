143a98bda14594f6c1e2de4907fa64d56c74663b
Ryan Arqueza <arquezaryan@gmail.com>
2021-09-02 22:40:02 +0800

    Add quote generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./style.css";
+import { data } from "./data.js";
+import logo from "./icons8-get-quote-50.png";
 function App() {
+  const secretArr = ["i", "lo", "ve", "co", "di", "ng"];
+
+  const [quote, setQuote] = useState(data[0]);
+  const [secret, setSecret] = useState(secretArr[0]);
+  const { img, author, quote: authorQuote } = quote;
+
+  const randomQuote = () => {
+    let randomizer = Math.floor(Math.random() * (7 - 0) - 0);
+    setQuote(data[randomizer]);
+    setSecret(secretArr[randomizer]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='body'>
+      <div className='container'>
+        <h1
+          className='title-page'
+          style={!secret ? { color: "yellow" } : { color: "white" }}
         >
-          Learn React
-        </a>
-      </header>
+          {secret ? secret : "Crack me genius!"}
+        </h1>
+        <div id='quote-box' className='quote-box'>
+          <img src={img} className='author-image' alt='author' />
+          <div id='text' className='quote-text'>
+            <img src={logo} alt='quote icon' /> {authorQuote}
+          </div>
+          <cite id='author' className='quote-author'>
+            - {author}
+          </cite>
+
+          <div className='buttons'>
+            <a
+              href={`https://twitter.com/intent/tweet?text=${authorQuote}-${author}`}
+              className='tweet-btn'
+              target='_blank'
+              id='tweet-quote'
+              rel='noreferrer'
+            >
+              <img
+                src='https://img.icons8.com/office/40/000000/twitter.png'
+                alt='twitter'
+              />
+            </a>
+            <button
+              id='new-qoute'
+              className='quote-btn btn'
+              onClick={randomQuote}
+            >
+              New Quote
+            </button>
+          </div>
+        </div>
+        <span
+          style={{
+            float: "right",
+            textTransform: "uppercase",
+            color: "white",
+            fontSize: ".75rem",
+            marginTop: "1em",
+          }}
+        >
+          by Ryan Arqueza
+        </span>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const data = [
+  {
+    img: "https://images.gr-assets.com/authors/1220982976p2/504377.jpg",
+    author: "Oscar Levant",
+    quote:
+      "There's a fine line between genius and insanity. I have erased this line.",
+  },
+  {
+    img: "https://images.gr-assets.com/authors/1390143800p2/2192.jpg",
+    author: "Aristotle",
+    quote: "No great mind has ever existed without a touch of madness.",
+  },
+  {
+    img: "https://images.gr-assets.com/authors/1422989334p2/9325.jpg",
+    author: "Gertrude Stein",
+    quote:
+      "It takes a lot of time to be a genius. You have to sit around so much, doing nothing, really doing nothing.",
+  },
+  {
+    img: "https://images.gr-assets.com/authors/1418930475p2/72605.jpg",
+    author: "Arthur Rimbaud",
+    quote: "Genius is the recovery of childhood at will.",
+  },
+  {
+    img: "https://images.gr-assets.com/authors/1371208630p2/13847.jpg",
+    author: "Charles Baudelaire",
+    quote: "Genius is nothing more nor less than childhood recaptured at will.",
+  },
+  {
+    img: "https://images.gr-assets.com/authors/1495008883p2/2448.jpg",
+    author: "Arthur Conan Doyle",
+    quote:
+      "Mediocrity knows nothing higher than itself; but talent instantly recognizes genius.",
+  },
+  {
+    img: "https://images.gr-assets.com/authors/1443341167p2/4615523.jpg",
+    author: "E.F. Schumacher",
+    quote:
+      "Any intelligent fool can make things bigger, more complex, and more violent. It takes a touch of genius — and a lot of courage to move in the opposite direction.",
+  },
+];
